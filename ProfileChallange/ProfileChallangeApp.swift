4390122d024a4db1e0ec0127e4fbaea9d38fc057
Cisino Junior <cisino.gomes@stone.com.br>
2023-06-04 14:25:32 -0300

    Initial Commit

---
+++ ProfileChallange/ProfileChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileChallangeApp.swift
+//  ProfileChallange
+//
+//  Created by Cisino Junior on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ProfileChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
